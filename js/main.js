ff8143cbae7b0218e515f922665091e9c961193a
Rohit-Kavitake <kavitakerohit2001@gmail.com>
2021-03-30 22:51:42 +0530

    Added support to save Notes

--- js/main.js
+++ js/main.js
@@ -34,16 +34,19 @@ function authStateObserver(user) {
         // Get the signed-in user's profile pic and name.
         var profilePicUrl = getProfilePicUrl() || "https://img.icons8.com/dusk/52/000000/cat-profile.png";
         var userName = getUserName();
-        console.log(firebase.auth().currentUser.email);
-        console.log(firebase.auth().currentUser.providerId);
+        // console.log(firebase.auth().currentUser.email);
+        // console.log(firebase.auth().currentUser.providerId);
         // Set the user's profile pic and name.
         document.querySelector('.profile').src = addSizeToGoogleProfilePic(profilePicUrl);  
         document.querySelector('.profile').removeAttribute('hidden');
         document.querySelector('#name').textContent = userName;
         document.querySelector('#signInbtn').setAttribute('hidden','true');
+        document.querySelector('#landing').setAttribute('hidden','true');
         document.querySelector("#signout").removeAttribute('hidden');
         document.querySelector("#signout").addEventListener('click',signOut);
         document.querySelector('#createbtn').removeAttribute('hidden');
+        document.querySelector("#sticky-footer").setAttribute('hidden','true');
+
     } 
     else { // User is signed out!
         console.log("User Logged Out")
@@ -52,6 +55,9 @@ function authStateObserver(user) {
         document.querySelector('#signout').setAttribute('hidden','true');
         document.querySelector('.profile').setAttribute('hidden','true');
         document.querySelector("#signInbtn").removeAttribute('hidden');
+        document.querySelector("#landing").removeAttribute('hidden');
+        document.querySelector("#sticky-footer").removeAttribute('hidden');
+        // document.querySelector("#sticky-footer").style.position = "absolute";
         document.querySelector('#createbtn').setAttribute('hidden','true');
     }
   }
@@ -62,19 +68,34 @@ function initFirebaseAuth() {
   
 }
 
-const started = () =>{
-    if(checksign()){
-        $('CreateBlog').modal()
-    }else{
-        signIn()
+const addTask = () =>{
+    let titlevalue = document.querySelector('#title').value;
+    let taskvalue = document.querySelector('#task').value;
+    let feedback = document.querySelector("#feedback");
+    if(title == "" || task == ""){
+        alert("Please Fill All Fields")
+    }
+    else{
+        firebase.firestore().collection(firebase.auth().currentUser.email).add({
+            title : titlevalue,
+            note : taskvalue,
+            time : firebase.firestore.FieldValue.serverTimestamp()
+        }).then(fun =>{
+            feedback.textContent = "Blog Successfully Saved";
+            document.querySelector('#reset').click();
+        setTimeout(function(){ feedback.textContent = "";document.querySelector('.close').click()},2000)
+        }).catch( err => {
+            console.log("Firestore Database adding data error")
+            feedback.textContent = "Blog Saving Error";
+            
+        });
+        // RetrieveData()
     }
 }
-
-
-
 
 
 initFirebaseAuth();
 
 
 document.querySelector("#signInbtn").addEventListener('click',signIn);
+
